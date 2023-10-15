7e2e2ae405d45af8ab4d175cf028a6eccf8f1986
Miras <miamib531@gmail.com>
2023-10-15 22:59:11 +0600

    deploy

--- src/components/CardGrid/CardGrid.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { CardGrid } from '.';
-
-describe('Card', () => {
-  it('should render error if there no cards', () => {
-    render(<CardGrid data={[]} />);
-    expect(screen.getByText(`No Search Results Found`)).toBeInTheDocument();
-  });
-});
--- src/constants/index.ts
+++
@@ -1,2 +0,0 @@
-export const LOCAL_STORAGE_KEY = 'superSecret';
-export const NO_SEARCH_RESULTS_MESSAGE = 'No Search Results Found';
